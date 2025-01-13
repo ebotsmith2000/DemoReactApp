16328c5eaa0a0fe4e3cac198791de3d4171912aa
ebotsmith2000 <ebotsmith@gmail.com>
2025-01-12 19:43:14 -0500

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~
-curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
+curl -fsSL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt update
 sudo apt install -y nodejs
